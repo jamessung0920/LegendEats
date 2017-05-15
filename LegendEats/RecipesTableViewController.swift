a5b407c40c92e7f90885cc76bc87caf94483934f
james <happyangry0920@gmail.com>
2017-05-15 14:37:21 +0800

    recipeView 修正

--- LegendEats/RecipesTableViewController.swift
+++ LegendEats/RecipesTableViewController.swift
@@ -41,6 +41,19 @@ class RecipesTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell: TableCell! = tableView.dequeueReusableCell(withIdentifier: identifier) as? TableCell
         cell.configurateTheCell(recipes[indexPath.row])
+        
+        cell.contentView.backgroundColor = UIColor.clear
+        
+        let whiteRoundedView : UIView = UIView(frame: CGRect(x: 10, y: 8, width: self.view.frame.size.width - 20, height: 300))
+        
+        whiteRoundedView.layer.backgroundColor = CGColor(colorSpace: CGColorSpaceCreateDeviceRGB(), components: [1.0, 1.0, 1.0, 0.9])
+        whiteRoundedView.layer.masksToBounds = false
+        whiteRoundedView.layer.cornerRadius = 10.0
+        whiteRoundedView.layer.shadowOffset = CGSize(width: -1, height: 3)
+        whiteRoundedView.layer.shadowOpacity = 0.2
+        
+        cell.contentView.addSubview(whiteRoundedView)
+        cell.contentView.sendSubview(toBack: whiteRoundedView)
         return cell!
     }
     
