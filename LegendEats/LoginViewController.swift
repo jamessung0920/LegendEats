dda5dc33a7b26213cd878fbc22ca2951293683f4
james <happyangry0920@gmail.com>
2017-06-09 13:55:12 +0800

    這次是真的臉濕濕了

--- LegendEats/LoginViewController.swift
+++ LegendEats/LoginViewController.swift
@@ -37,52 +37,55 @@ class LoginViewController: UIViewController {
     //Login Action
     @IBAction func loginAction(_ sender: AnyObject) {
         
-        if self.emailTextField.text == "" || self.passwordTextField.text == "" {
-            
-            //Alert to tell the user that there was an error because they didn't fill anything in the textfields because they didn't fill anything in
-            
-            let alertController = UIAlertController(title: "Error", message: "Please enter an email and password.", preferredStyle: .alert)
+        if self.blockStudentNumberArray.contains(self.emailTextField.text!)
+        {
+            let alertController = UIAlertController(title: "Error", message: "Your account have been blocked!!", preferredStyle: .alert)
             
             let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
             alertController.addAction(defaultAction)
             
             self.present(alertController, animated: true, completion: nil)
             
-        }
-        else
-        {
+        } else {
             
-            FIRAuth.auth()?.signIn(withEmail: self.emailTextField.text!, password: self.passwordTextField.text!) { (user, error) in
-                
-                if error == nil {
-                    
-                    //Print into the console if successfully logged in
-                    print("You have successfully logged in")
-                    
-                    //Go to the RecipesTableViewController if the login is sucessful
-                    let vc = self.storyboard?.instantiateViewController(withIdentifier: "TabBar")
-                    self.present(vc!, animated: true, completion: nil)
-                    
-                } else {
-                    
-                    //Tells the user that there is an error and then gets firebase to tell them the error
-                    let alertController = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: .alert)
-                    
-                    let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
-                    alertController.addAction(defaultAction)
-                    
-                    self.present(alertController, animated: true, completion: nil)
-                }
-            }
-            if self.blockStudentNumberArray.contains(self.emailTextField.text!)
+            if self.emailTextField.text == "" || self.passwordTextField.text == ""
             {
-                    let alertController = UIAlertController(title: "Error", message: "Your account have been blocked!!", preferredStyle: .alert)
+                
+                //Alert to tell the user that there was an error because they didn't fill anything in the textfields because they didn't fill anything in
+                
+                let alertController = UIAlertController(title: "Error", message: "Please enter an email and password.", preferredStyle: .alert)
+                
+                let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                alertController.addAction(defaultAction)
+                
+                self.present(alertController, animated: true, completion: nil)
+                
+            } else {
+                
+                FIRAuth.auth()?.signIn(withEmail: self.emailTextField.text!, password: self.passwordTextField.text!) { (user, error) in
                     
-                    let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
-                    alertController.addAction(defaultAction)
-                    
-                    self.present(alertController, animated: true, completion: nil)
+                    if error == nil {
+                        
+                        //Print into the console if successfully logged in
+                        print("You have successfully logged in")
+                        
+                        //Go to the RecipesTableViewController if the login is sucessful
+                        let vc = self.storyboard?.instantiateViewController(withIdentifier: "TabBar")
+                        self.present(vc!, animated: true, completion: nil)
+                        
+                    } else {
+                        
+                        //Tells the user that there is an error and then gets firebase to tell them the error
+                        let alertController = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: .alert)
+                        
+                        let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                        alertController.addAction(defaultAction)
+                        
+                        self.present(alertController, animated: true, completion: nil)
+                    }
+                }
             }
         }
     }
+    
 }
